420be2224a165298dc209e27a57fe00f6c192ad1
Hebert Porto <hebertporto@gmail.com>
2014-06-29 16:53:30 -0300

    Adicionado favicon + criacao do menu principal em um arquivo separado e biblitecbiblioteca Jquery.min

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+    <html lang="pt-br">
+        <head>
+            <meta charset="utf-8">
+            <link href="css/bootstrap.min.css" rel="stylesheet">
+            <link href="/images/favicon.ico" rel="icon" type="image/x-icon" />
+            <title>PHP: FOUNDATION</title>
+        </head>
+    <body>
+    <header>
+        <?php require_once("menu_principal.php"); ?>
+    </header>
+    <footer></footer>
+    <script src="js/1.11.1-jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
